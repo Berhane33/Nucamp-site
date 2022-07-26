473cf7817bbce09ea8e4b41d2b89958ad7ac6595
Jacob Messou <jacob.messou@gmail.com>
2022-07-26 00:46:05 -0700

    AnimatedDisplayCard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='contact' element={<ContactPage />} />
-        <Route path='directory' element={<CampsitesDirectoryPage />}/>
-        <Route path='directory/:campsiteId' element={<CampsiteDetailPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
