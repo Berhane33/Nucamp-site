87c5f5d3e2620c7b1544bc4dd73d4dda5aea9d84
Jacob Messou <jacob.messou@gmail.com>
2022-08-04 22:33:57 -0700

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import campsitesReducer from '../features/counter/counterSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
@@ -12,4 +13,5 @@ export const store = configureStore({
     partners: partnersReducer,
     promotions: promotionsReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
